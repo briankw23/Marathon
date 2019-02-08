72421b32c3aecacb065d3ea68f1fde27145c56ee
brian <briankw23@gmail.com>
2019-02-08 17:02:30 -0600

    wip

---
+++ Marathon/DataAccess/RunStorage.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Marathon.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marathon.DataAccess
+{
+    public class RunStorage
+    {
+        private readonly string ConnectionString;
+
+        public RunStorage(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+        //APIS
+        public List<Run> GetSingle()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.Query<Run>(@"select * from Run");
+
+                return result.ToList();
+            }
+        }
+    }
+}
