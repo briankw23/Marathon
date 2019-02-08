e3cc7608ea197f556058b8888e8024ce0c394ff0
brian <briankw23@gmail.com>
2019-02-08 17:19:56 -0600

    weights GET working

---
+++ Marathon/Controllers/WeightsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Marathon.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Marathon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeightsController : ControllerBase
+    {
+        private readonly WeightStorage _storage;
+
+        public WeightsController(IConfiguration config)
+        {
+            _storage = new WeightStorage(config);
+        }
+
+        // GET ALL Weights
+        [HttpGet]
+        public IActionResult GetTheOnlyUser()
+        {
+            return Ok(_storage.GetAllWeightsTasks());
+        }
+    }
+}
---
+++ Marathon/DataAccess/WeightStorage.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Marathon.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marathon.DataAccess
+{
+    public class WeightStorage
+    {
+        private readonly string ConnectionString;
+
+        public WeightStorage(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+        //APIS
+        //GET ALL Weights
+        public List<Weights> GetAllWeightsTasks()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.Query<Weights>(@"select * from Weights");
+
+                return result.ToList();
+            }
+        }
+    }
+}
