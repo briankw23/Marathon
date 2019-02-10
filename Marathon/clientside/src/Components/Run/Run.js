6113e114a9cf530f10855c19b023d25a96b69e79
brian <briankw23@gmail.com>
2019-02-09 20:44:18 -0600

    wip

--- Marathon/clientside/src/Components/Run/Run.js
+++ Marathon/clientside/src/Components/Run/Run.js
@@ -6,7 +6,28 @@ class Run extends React.Component {
         const { details } = this.props;
         return (
             <div className='Run'>
-                <h1>{details.name}</h1>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">{details.name}</h3>
+                        <div>
+                            Done: {details.complete}<span className='glyphicon glyphicon-remove-circle'></span><span className='glyphicon glyphicon-ok-circle'></span>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div>
+                            Date: {details.date}
+                        </div>
+                        <div>
+                            Details:{details.description}
+                        </div>
+                        <div>
+                            Target: {details.targetMiles}
+                        </div>
+                        <div>
+                            Actual: {details.actualMiles}
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     };
